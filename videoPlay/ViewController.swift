7fa3c00ca8880bd1f2f8259bbbf4837a527c3e4b
akaravind <aravind.t@cogzidel.com>
2018-06-01 17:38:35 +0530

    pick video from gallery and play it.

---
+++ videoPlay/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  videoPlay
+//
+//  Created by apple on 2/23/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController,UIAlertViewDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIPopoverControllerDelegate{
+//    @IBOutlet weak var video: UIButton!
+//
+//    @IBOutlet weak var videoView: UIView!
+//
+//     var videoURL: NSURL? = nil
+//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//
+//
+////
+////        let videoURL = NSURL(string: "VideoUr")
+////        let player = AVPlayer(url: videoURL! as URL)
+////        let playerViewController = AVPlayerViewController()
+////        playerViewController.player = player
+////        self.present(playerViewController, animated: true) {
+////            playerViewController.player!.play()
+////        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    @IBAction func videoBtn(_ sender: Any) {
+        
+ 
+//        getVideos()
+        
+        
+        
+        //        let videoURL = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        //        let player = AVPlayer(url: videoURL!)
+        //        let playerViewController = AVPlayerViewController()
+        //        playerViewController.player = player
+        //        self.present(playerViewController, animated: true) {
+        //            playerViewController.player!.play()
+        //        self.performSegue(withIdentifier: "gotosecond", sender: self)
+    }
+    
+//    func getVideos()
+//
+//    {
+//
+//        let imagePickerController = UIImagePickerController()
+//
+//
+//        imagePickerController.sourceType = .savedPhotosAlbum
+//
+//
+//        imagePickerController.mediaTypes = [kUTTypeMovie as String]
+//
+//
+//        imagePickerController.delegate = self
+//
+//
+//        self.present(imagePickerController, animated: true, completion: nil)
+//
+//    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
